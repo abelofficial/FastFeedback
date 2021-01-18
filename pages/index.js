e01520a4e6c368703113b3b660fa6bb38225c6ae
Abel Sintaro <abelofficial94@gmail.com>
2021-01-18 09:34:33 +0100

    Add the starter configurations.

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,32 @@
+import { Button, Code, Heading, Text } from '@chakra-ui/react';
 import Head from 'next/head';
-import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
+
+import { useAuth } from '@lib/auth';
 
 export default function Home() {
   const auth = useAuth();
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Fast feedback</title>
       </Head>
+      <main>
+        <Heading>Fast Feedback</Heading>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback</h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        {auth?.user ? (
-          <button onClick={(e) => auth.signout()}>Signout</button>
+        {auth.user ? (
+          <>
+            <Text>
+              The current user is <Code>{auth.user.name} </Code>
+            </Text>
+            <Button onClick={(e) => auth.signout()}>Signout</Button>
+          </>
         ) : (
-          <button onClick={(e) => auth.signinWithGithub()}>SignIn</button>
+          <>
+            <Text>No signed in user</Text>
+            <Button onClick={(e) => auth.signinWithGithub()}>SignIn</Button>
+          </>
         )}
-
-        <div>{auth?.user?.email}</div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
